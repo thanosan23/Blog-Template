cf41dcf79fd23bb4697ca973d89b6346e1b0d4d9
thanosan23 <p.thanosan23@gmail.com>
2023-07-27 20:31:19 -0400

    Added footer

--- src/pages/blog1.tsx
+++ src/pages/blog1.tsx
@@ -2,21 +2,24 @@ import Head from "next/head";
 import Link from "next/link";
 
 export default function Blog1() {
-  return (
-    <>
-      <Head>
-        <title>Blog1</title>
-        <meta name="description" content="" />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
-      </Head>
-      <main className="min-h-screen bg-zinc-900 text-white p-20">
-          <Link href="/"><button className="hover:opacity-80 opacity-100 transition hover:-translate-y-1 bg-zinc-700 rounded py-2 px-5">Back</button></Link>
-          <div className="flex flex-col gap-8 ">
-              <h1 className="font-bold text-6xl pt-3 mt-3">My First Blog</h1>
-              <p className="text-zinc-300 italic">July 27, 2023</p>
-              <p className="text-lg">Welcome to my first blog! This blog is about programming!</p>
-          </div>
-      </main>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>Blog1</title>
+                <meta name="description" content="" />
+                {/* <link rel="icon" href="/favicon.ico" /> */}
+            </Head>
+            <main className="min-h-screen bg-zinc-900 text-white p-20">
+                <Link href="/"><button className="hover:opacity-80 opacity-100 transition hover:-translate-y-1 bg-zinc-700 rounded py-2 px-5">Back</button></Link>
+                <div className="flex flex-col gap-8 ">
+                    <h1 className="font-bold text-6xl pt-3 mt-3">My First Blog</h1>
+                    <p className="text-zinc-300 italic">July 27, 2023</p>
+                    <p className="text-lg">Welcome to my first blog! This blog is about programming!</p>
+                </div>
+            </main>
+            <footer className="flex justify-center pb-5 text-white bg-zinc-900">
+                <p>View the source code <Link className="underline" href='https://github.com/thanosan23/Blog-Template'>here!</Link></p>
+            </footer>
+        </>
+    );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,34 +10,36 @@ const blogs = [
 ];
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Blog</title>
-        <meta name="description" content="" />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
-      </Head>
-      <main className="min-h-screen flex flex-col gap-8 bg-zinc-900 text-white p-20">
-          <h1 className="font-bold text-6xl pt-3 mt-3">Hi, I'm Thanosan!</h1>
-          <p className="text-2xl">Welcome to my blog! This blog will be about all things programming!</p>
+    return (
+        <>
+            <Head>
+                <title>Blog</title>
+                <meta name="description" content="" />
+                {/* <link rel="icon" href="/favicon.ico" /> */}
+            </Head>
+            <main className="min-h-screen flex flex-col gap-8 bg-zinc-900 text-white p-20">
+                <h1 className="font-bold text-6xl pt-3 mt-3">Hi, I&#39;m Thanosan!</h1>
+                <p className="text-2xl">Welcome to my blog! This blog will be about all things programming!</p>
 
-          <div className="flex gap-3">
-              {blogs.map(({title, about, link}, idx) =>
-                  <div key={idx} className="hover:-translate-y-1 transition flex flex-col gap-3 bg-zinc-800 p-5 w-1/3 rounded-xl">
-                      <p className="font-bold text-center text-lg">{title}</p>
-                      <p>
-                          {about}
-                      </p>
-                      <div className="flex justify-center">
-                          <Link className="flex justify-center w-1/2" href={link}>
-                            <button className="p-3 hover:opacity-70 opacity-100 bg-zinc-700 rounded">View Blog</button>
-                          </Link>
-                      </div>
-                  </div>
-              )}
-          </div>
-
-      </main>
-    </>
-  );
+                <div className="flex gap-3">
+                    {blogs.map(({title, about, link}, idx) =>
+                    <div key={idx} className="hover:-translate-y-1 transition flex flex-col gap-3 bg-zinc-800 p-5 w-1/3 rounded-xl">
+                        <p className="font-bold text-center text-lg">{title}</p>
+                        <p>
+                            {about}
+                        </p>
+                        <div className="flex justify-center">
+                            <Link className="flex justify-center w-1/2" href={link}>
+                                <button className="p-3 hover:opacity-70 opacity-100 bg-zinc-700 rounded">View Blog</button>
+                            </Link>
+                        </div>
+                    </div>
+                              )}
+                </div>
+            </main>
+            <footer className="flex justify-center pb-5 text-white bg-zinc-900">
+                <p>View the source code <Link className="underline" href='https://github.com/thanosan23/Blog-Template'>here!</Link></p>
+            </footer>
+        </>
+    );
 }
